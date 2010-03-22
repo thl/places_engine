a092290f9e57d278c9561524f7810e61107c4ca8
Tom Benner <tbb4n@virginia.edu>
2010-03-22 14:37:09 -0400

    Adding google_maps_key back into ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -353,5 +353,14 @@ module ApplicationHelper
        return 'http://www.thlib.org'
      end
    end
-  
+   
+   def google_maps_key
+     hostname = Socket.gethostname.downcase
+     if hostname== 'e-bhutan.bt'
+       'ABQIAAAA-y3Dt_UxbO4KSyjAYViOChQYlycRhKSCRlUWwdm5YkcOv9JZvxQ7K1N-weCz0Vvcplc8v8TOVZ4lEQ'
+     else
+       'ABQIAAAAmlH3GDvD6dTOdZjfrfvLFxTkTKGJ2QQt6wuPk9SnktO8U_sCzxTyz_WwKoSJx63MPLV9q8gn8KCNtg'
+     end
+   end
+     
 end
