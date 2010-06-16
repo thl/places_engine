2c583e5946cf2c5d99da70f5189b0258452945b8
Andres Montano <amontano@virginia.edu>
2010-06-16 03:06:56 -0600

    caching category feature counts for places integration within kmaps

--- init.rb
+++ init.rb
@@ -1,4 +1,6 @@
 # Include hook code here
+require 'postgres_patch'
+require File.join(File.dirname(__FILE__), 'app', 'sweepers', 'cached_category_count_sweeper')
 require 'active_record_ext'
 require 'array_ext'
 require 'will_paginate_ext'
