e5fb0644ff0b9bf0161ca69e86ad0f8564299ce7
Tom Benner <tbb4n@virginia.edu>
2010-08-14 10:27:56 -0400

    Setting up universal navigation views for media and topics

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,9 +2,13 @@ class MediaController < ApplicationController
   # GET /media/1
   # GET /media/1.xml
   def show
+    set_common_variables(session)
     @medium = Medium.find(params[:id])
     @features = Feature.find_all_by_medium_id(@medium.id)
-    @title = "Features Associated to Medium #{@medium.id}"
+    @title = "Places Associated with Medium #{@medium.id}"
+    @tab_title = "Media"
+    
+    @feature = Feature.find(session[:interface][:context_id]) unless session[:interface][:context_id].blank?
     respond_to do |format|
       format.html { render :template => 'features/list' } # show.html.erb
     end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,10 +2,14 @@ class TopicsController < ApplicationController
   # GET /topics/1
   # GET /topics/1.xml
   def show
+    set_common_variables(session)
     @category = Category.find(params[:id])
     join = @category.root.id==20 ? :feature_object_types : :category_features    
     @features = Feature.all(:conditions => {'category_features.category_id' => @category.id}, :joins => join)
-    @title = "Features Associated to #{@category.title}"
+    @title = "Places Associated with #{@category.title}"
+    @tab_title = "Topics"
+    
+    @feature = Feature.find(session[:interface][:context_id]) unless session[:interface][:context_id].blank?
     respond_to do |format|
       format.html { render :template => 'features/list' } # show.html.erb
     end
