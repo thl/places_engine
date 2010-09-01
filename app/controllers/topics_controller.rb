fd045b1d1db46310206e5c2a583b4a4fddf26e32
Tom Benner <tbb4n@virginia.edu>
2010-09-01 17:12:14 -0400

    Ajaxifying the list of features for a topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,16 +3,22 @@ class TopicsController < ApplicationController
   # GET /topics/1.xml
   def show
     set_common_variables(session)
+
     @category = Category.find(params[:id])
-    join = @category.root.id==20 ? :feature_object_types : :category_features    
-    @features = Feature.all(:conditions => {'category_features.category_id' => @category.id}, :joins => join)
     @object_type = "Topic"
     @object_title = @category.title
     @object_url = Category.element_url(@category.id, :format => 'html')
-    
+
+    join = @category.root.id==20 ? :feature_object_types : :category_features
+    @features = Feature.paginate(:conditions => {'category_features.category_id' => @category.id, 'cached_feature_names.view_id' => current_view.id}, :joins => join, :include => {:cached_feature_names => :feature_name}, :order => 'feature_names.name', :page => params[:page] || 1, :per_page => 15)
     @feature = Feature.find(session[:interface][:context_id]) unless session[:interface][:context_id].blank?
-    respond_to do |format|
-      format.html { render :template => 'features/list' } # show.html.erb
+
+    if request.xhr?
+      render :partial => 'features/list'
+    else
+      respond_to do |format|
+        format.html { render :template => 'features/list' }
+      end
     end
   end
 end
