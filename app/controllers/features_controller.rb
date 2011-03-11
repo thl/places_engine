1f1476b22c15901c03c90bf663c79bf39292de04
Jed Verity <jev3a@virginia.edu>
2011-03-11 16:31:06 -0500

    adding caching for node_tree_expanded

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,7 +1,10 @@
 class FeaturesController < ApplicationController
   caches_page :show, :if => :api_response?.to_proc
-  
+  caches_action :node_tree_expanded, :cache_path => :tree_cache_path.to_proc, :if => Proc.new { |c| c.request.xhr? }
   #
+  def tree_cache_path
+    "#{current_perspective.id}/#{current_view.id}/node_id_#{params[:id]}"
+  end
   #
   #
   def index
