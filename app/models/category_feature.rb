c7b81523b438b6f1359d6157acaaacec5d75500d
Andres Montano <amontano@virginia.edu>
2011-06-29 12:32:27 -0600

    handling show_parent and show_view in importation

--- app/models/category_feature.rb
+++ app/models/category_feature.rb
@@ -78,8 +78,8 @@ end
 #  perspective_id :integer
 #  numeric_value  :integer
 #  position       :integer         not null, default(0)
-#  show_parent    :boolean
-#  show_root      :boolean         default(TRUE)
+#  show_parent    :boolean         not null
+#  show_root      :boolean         not null, default(TRUE)
 #  string_value   :string(255)
 #  type           :string(255)
 #  created_at     :timestamp
