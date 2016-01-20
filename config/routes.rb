63e9a958deace8ad9c2579b6433df1ed817871cb
Andres Montano <amontano@virginia.edu>
2016-01-19 23:51:13 -0600

    fixing priorization of feature types and shapes, also adding deletion of shapes

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
       resources :time_units do
         get :new_form, on: :collection
       end
+      post :set_priorities, on: :collection
     end
   end
   resources :altitudes do
@@ -63,6 +64,7 @@ Rails.application.routes.draw do
   end
   resources :feature_object_types do
     resources :notes, :citations
+    post :set_priorities, on: :collection
   end
   resources :shapes do
     resources :notes, :citations
