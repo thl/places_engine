531acf01bdbcc4b319dcef0b77fcab098ae41d6a
Derik Ramirez <drc2r@virginia.edu>
2017-07-14 06:53:35 +0100

    Fix errors reported in MANU-3973

--- app/controllers/admin/category_features_controller.rb
+++ app/controllers/admin/category_features_controller.rb
@@ -10,20 +10,20 @@ class Admin::CategoryFeaturesController < AclController
     errors = []
     @feature = Feature.find(params[:feature_id])
     if mca_cats.nil?
-      redirect_to admin_feature_url(@feature)
+      redirect_to admin_feature_url(@feature.fid)
     elsif mca_cats.size==1
       mca_hash[:category_id] = mca_cats.first
       @cf = @feature.category_features.new(mca_hash)
       respond_to do |format|
         if @cf.save
-          format.html { redirect_to admin_feature_url(@feature) }
+          format.html { redirect_to admin_feature_url(@feature.fid) }
         else
           format.html { render :action => "new" }
         end
       end
     else
       mca_cats.each { |c_id| @feature.category_features.create(:category_id => c_id) }
-      redirect_to admin_feature_url(@feature)
+      redirect_to admin_feature_url(@feature.fid)
     end    
   end
   
