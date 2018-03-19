0dbb4967331a2483d98c03dfbfd08e21c15dcae1
rderik <rcderik@gmail.com>
2018-03-19 16:33:57 -0600

    Adds citation to shapes
    Add the capability to shapes to add citations via the editorial
    interface.

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
       end
     end
     resources :shapes do
+      resources :citations
       resources :notes do
         get :add_author, on: :collection
       end
