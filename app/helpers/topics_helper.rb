eaa645ced195f58291236eb4b23c896f1c5593ac
Derik Ramirez <drc2r@virginia.edu>
2018-07-17 09:44:32 +0100

    Improved display of related subjects
    Create a distinct separation between feature_types and subjects on the
    related Subjects interface.
    
    Refactor code for related subjects(topics)
    
    Removed logic from the view to helpers and add support for the kmaps
    pop-up for the associated topics.
    
    Update engine version for merge to master

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,19 @@
 module TopicsHelper
+  def category_feature_list(category_features)
+    category_list = ""
+    category_features.each do |cf|
+      c = cf.category
+      if !c.nil?
+        values = []
+        values << cf.string_value if !cf.string_value.blank?
+        values << cf.numeric_value if !cf.numeric_value.nil?
+        stack = cf.category_stack
+        stack.push(link_to(stack.pop, c.get_url(nil, :format => '')))
+        topic_stack = "#{stack.join(' > ')}: "
+        topic_stack += " #{values.join(', ')}" if !values.empty?
+        category_list += content_tag(:li, "#{topic_stack} #{note_popup_link_for(cf)} #{time_units_for(cf)}".html_safe)
+      end
+    end
+    category_list
+  end
 end
