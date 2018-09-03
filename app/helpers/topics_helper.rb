0438a14f36454370886270a8ee4c8c59db80a303
rderik <rderik@gmail.com>
2018-09-03 18:54:58 +0100

    Improve display for related subjects for places - MANU-5129 (#12)
    * Improve display for related subjects for places
    
    Concistently display headers and colons only when content exists, and
    improve display format for headers.
    
    * Improved presentation, removed extra spaces on topics_helper
    
    * Improve related subjects display for dates
    
    When displaying dates now it properly removes colons to improve
    readability.

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -9,9 +9,10 @@ module TopicsHelper
         values << cf.numeric_value if !cf.numeric_value.nil?
         stack = cf.category_stack
         stack.push(link_to(stack.pop, c.get_url(nil, :format => '')))
-        topic_stack = "#{stack.join(' > ')}: "
+        topic_stack = "#{stack.join(' > ')}"
+        topic_stack += ":" if !values.empty?
         topic_stack += " #{values.join(', ')}" if !values.empty?
-        category_list += content_tag(:li, "#{topic_stack} #{note_popup_link_for(cf)} #{time_units_for(cf)}".html_safe)
+        category_list += content_tag(:li, "#{topic_stack} #{time_units_for(cf)} #{note_popup_link_for(cf)}".html_safe)
       end
     end
     category_list
