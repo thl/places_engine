6ccc515ff2d49c23c16102861c9fc7e3e68e3e34
Andres Montano <amontano@virginia.edu>
2018-10-26 16:42:44 -0600

    Fixing MANU-3943: citation associations with feature object types (polymorphic does not behave well with STI)

--- lib/places_engine/extension/citations_controller.rb
+++ lib/places_engine/extension/citations_controller.rb
@@ -4,7 +4,7 @@ module PlacesEngine
       extend ActiveSupport::Concern
 
       included do
-        belongs_to :caption, :description, :feature_geo_code, :feature_name, :feature_name_relation, :feature_relation, :summary, #This list comes from the CitationsController in kmaps_engine
+        belongs_to :caption, :description, :feature, :feature_geo_code, :feature_name, :feature_name_relation, :feature_relation, :summary, #This list comes from the CitationsController in kmaps_engine
         :altitude, :category_feature, :feature_object_type, :shape #specific to places_engine
       end
     end
