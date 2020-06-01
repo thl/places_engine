ce5a5ab1a48a6d5d53291414bdcde8a163a6503b
Andres Montano <amontano@virginia.edu>
2020-06-01 11:00:31 -0600

    fetching asset index url from shanti integration source

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,4 @@
 class Topic < SubjectsIntegration::Feature
-  headers['Host'] = SubjectsIntegration::Feature.headers['Host'] if !SubjectsIntegration::Feature.headers['Host'].blank?
   self.element_name = 'feature'
     
   def features(options = {})
