d6ac2b39dc2a8f116e13b015d2f36de3f7525990
Andres Montano <amontano@virginia.edu>
2020-10-08 15:21:24 -0500

    updating links to other apps

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -6,14 +6,14 @@ class MediaController < ApplicationController
     if @medium.nil?
       redirect_to features_url
     else
-      @features = Feature.where('features.fid' => MmsIntegration::Medium.find(@medium.id).feature_ids, 'cached_feature_names.view_id' => current_view.id).includes(:cached_feature_names => :feature_name).references(:cached_feature_names => :feature_name).paginate(:page => params[:page] || 1, :per_page => 15).order('feature_names.name')
+      @features = Feature.where('features.fid' => MmsIntegration::Medium.find(@medium.id).feature_ids, 'cached_feature_names.view_id' => current_view.id).includes(cached_feature_names: :feature_name).references(cached_feature_names: :feature_name).paginate(page: params[:page] || 1, per_page: 15).order('feature_names.name')
       @object_type = "Media"
       @object_title = "Medium #{@medium.id}"
-      @object_url = MmsIntegration::Medium.element_url(@medium.id, :format => 'html')
+      @object_url = MmsIntegration::Medium.element_url(@medium.id, format: '')
       @feature = Feature.find(session['interface']['context_id']) unless session['interface'].blank? || session['interface']['context_id'].blank?
       respond_to do |format|
-        format.html { render :template => 'features/list' } # show.html.erb
-        format.js   { render :template => 'features/paginated_list' }
+        format.html { render template: 'features/list' } # show.html.erb
+        format.js   { render template: 'features/paginated_list' }
       end
     end
   end
