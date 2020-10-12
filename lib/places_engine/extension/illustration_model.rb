a5d18033a3ae4cca8f737dd27b79515ac3bdd3b7
Andres Montano <amontano@virginia.edu>
2020-10-12 14:03:37 -0500

    fixing bug of communication with MMS

--- lib/places_engine/extension/illustration_model.rb
+++ lib/places_engine/extension/illustration_model.rb
@@ -8,8 +8,8 @@ module PlacesEngine
       
       def place
         pic = self.picture
-        fid = pic.instance_of?(ExternalPicture) ? pic.place_id : pic.locations.first.to_i
-        fid.nil? ? nil : Feature.get_by_fid(fid)
+        fid = pic.instance_of?(ExternalPicture) ? pic.place_id : pic.locations.first
+        fid.nil? ? nil : Feature.get_by_fid(fid.to_i)
       end
             
       module ClassMethods
